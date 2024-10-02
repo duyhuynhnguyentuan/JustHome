6ddb31b7c6df94d7e78cbfc68e92a5aaaf4597cd
duyhuynhnguyentuan <andyhntd2003@gmail.com>
2024-10-02 16:59:48 +0700

    Initial Commit

---
+++ JustHome/JustHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustHomeApp.swift
+//  JustHome
+//
+//  Created by Huynh Nguyen Tuan Duy on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct JustHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JustHomeTests/JustHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustHomeTests.swift
+//  JustHomeTests
+//
+//  Created by Huynh Nguyen Tuan Duy on 2/10/24.
+//
+
+import Testing
+@testable import JustHome
+
+struct JustHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
